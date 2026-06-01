ab3f8c1815e514e28947228b32325d141d3a71f0
santoselizabeth <elizabethpe.santos@gmail.com>
2026-06-01 19:27:26 -0300

    chore: configurada porta dinamica para o deploy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import Meta from './meta.js'; // Mantido no singular conforme seu arquivo
 import Usuario from './usuario.js';   
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
