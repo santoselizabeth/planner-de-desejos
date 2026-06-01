b0f4926b2cbf68133d3545d42cb61dff02d2dab9
santoselizabeth <elizabethpe.santos@gmail.com>
2026-06-01 20:07:18 -0300

    fix: limpando cache e aplicando gitignore corretamente

--- usuario.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const usuarioSchema = new mongoose.Schema({
-    nome: { type: String, required: true },
-    email: { type: String, required: true, unique: true }, 
-    senha: { type: String, required: true }
-});
-
-export default mongoose.model('Usuario', usuarioSchema);
